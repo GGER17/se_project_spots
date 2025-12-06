91b7498c25e7df4a7847acf12ca49eb6d821791f
Grecia Espinoza <g3901@icloud.com>
2025-12-05 21:28:26 -0600

    Spots Stage 6 (with Code Review): Form Submission

--- scripts/index.js
+++ scripts/index.js
@@ -37,8 +37,56 @@ const newPostButton = document.querySelector(".profile__button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
+//const profileTitle= document.querySelector(".profile__title"); for profile edit works.
+const profileTitle = document.querySelector(".profile__title"); // Gets "Bessie Coleman"
+const profileText = document.querySelector(".profile__text"); // Gets "Civil Aviator"
+const profileNameInput = editProfileModal.querySelector("#profile-name-input"); // The name input field
+const descriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+); // The description input field
+
+const cardCaptionInput = newPostModal.querySelector("#card-caption-input"); //Targets the caption input
+const linkInput = newPostModal.querySelector("#card-image-input"); //Targets the link input
+
+const addCardFormElement = newPostModal.querySelector("#newpost-form"); //The card input field
+
+const profileFormElement = editProfileModal.querySelector("#edit-form");
+
+//created the submission form handler.
+function handleProfileFormSubmit(evt) {
+  // Step 1: Prevent default behavior
+  // Prevent default browser behavior.
+  evt.preventDefault();
+  // Step 2: Update profile display elements
+  profileTitle.textContent = profileNameInput.value;
+  profileText.textContent = descriptionInput.value;
+  // Step 3: Close the modal
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+//Have set the submit listener.
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+//created the submission form handler.
+function handleAddCardSubmit(evt) {
+  // Step 1: Prevent default behavior
+  // Prevent default browser behavior.
+  evt.preventDefault();
+  // Step 2: Log both input values to the console
+  console.log(cardCaptionInput.value);
+  console.log(linkInput.value);
+  // Step 3: Close the modal
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+// Create the submit listener.
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
 editProfileButton.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+
+  profileNameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileText.textContent;
 });
 
 editProfileCloseButton.addEventListener("click", function () {
@@ -57,3 +105,8 @@ newPostCloseButton.addEventListener("click", function () {
 initialCards.forEach((element) => {
   console.log(element.name);
 });
+
+/*holding bar for laters maybe.
+const Title = document.querySelector(".profile__title");
+const profileText = document.querySelector(".profile__text");
+*/
